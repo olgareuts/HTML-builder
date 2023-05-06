50ab4da12d8a5f5b6e2ff1a57206af17f315690c
olgareuts <olga.re@mail.ru>
2023-05-06 22:27:36 +0300

    feat: add 05-merge-styles solution

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+
+const folderPath = path.join(__dirname, 'styles');
+const filePath = path.join(__dirname, 'project-dist', 'bundle.css');
+
+fs.readdir(folderPath, { withFileTypes: true }, (error, files) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const writeStream = fs.createWriteStream(filePath);
+    files.forEach(file => {
+      const fileCopyPath = path.join(__dirname, 'styles', file.name);
+      if (!file.isDirectory() && path.parse(fileCopyPath).ext === '.css') {
+        fs.readFile(fileCopyPath, 'utf8', function(error, data) { 
+          if (error) {
+            console.log(error);
+          } else {
+            writeStream.write(data + '\n');
+          }
+        });
+      }
+    });
+  }
+});
