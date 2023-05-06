1d74b005e65095616f997a68a8c9ae79f3d6c981
olgareuts <olga.re@mail.ru>
2023-05-06 15:26:42 +0300

    feat: add 03-files-in-folder solution

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+
+fs.readdir(path.join(__dirname, 'secret-folder'), (error, files) => {
+  if (error) {
+    console.log(error);
+  } else {
+    files.forEach(file => {
+      const filePath = path.join(__dirname, 'secret-folder', file);
+      fs.stat(filePath, (error, stats) => {
+        if (error) {
+          console.log(error);
+        } else {
+          if (stats.isFile()) {
+            console.log(`${path.parse(filePath).name} - ${path.parse(filePath).ext.slice(1)} - ${(stats.size/1024)}kb`);
+          }
+        }
+      });
+    });
+  }
+});
