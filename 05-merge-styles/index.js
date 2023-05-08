14af60591d6714909c1f65676f8eb09b22795031
olgareuts <olga.re@mail.ru>
2023-05-08 13:10:17 +0300

    refactor: change variable names

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,18 +1,18 @@
 const fs = require('fs');
 const path = require('path');
 
-const folderPath = path.join(__dirname, 'styles');
-const filePath = path.join(__dirname, 'project-dist', 'bundle.css');
+const stylesFolder = path.join(__dirname, 'styles');
+const outputCSSFile = path.join(__dirname, 'project-dist', 'bundle.css');
 
-fs.readdir(folderPath, { withFileTypes: true }, (error, files) => {
+fs.readdir(stylesFolder, { withFileTypes: true }, (error, files) => {
   if (error) {
     console.log(error);
   } else {
-    const writeStream = fs.createWriteStream(filePath);
+    const writeStream = fs.createWriteStream(outputCSSFile);
     files.forEach(file => {
-      const fileCopyPath = path.join(__dirname, 'styles', file.name);
-      if (!file.isDirectory() && path.parse(fileCopyPath).ext === '.css') {
-        fs.readFile(fileCopyPath, 'utf8', function(error, data) { 
+      const filePath = path.join(stylesFolder, file.name);
+      if (!file.isDirectory() && path.parse(filePath).ext === '.css') {
+        fs.readFile(filePath, 'utf8', function(error, data) { 
           error ? console.log(error) : writeStream.write(data + '\n');
         });
       }
